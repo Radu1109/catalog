750799517186b32c858fbeb38c201beef9f7aa18
Radu <radumihai179@yahoo.com>
2025-08-31 15:32:04 +0300

    Prima versiune a catalogului

---
+++ main.py
@@ -0,0 +1,16 @@
+import functii
+
+'''
+COD BEGINNER FRIENDLY DE REIMPOSPATARE MEMORIE
+CATALOG DE STUDENTI
+1. Input de la user cu : nume, prenume, nota
+2.Adaugam intr un tabel SQL
+3.Functii: Afisare studenti, cauta dupa nume, calculeaza media studentului/clasei
+'''
+
+def main():
+    functii.Meniu()
+
+if __name__ == "__main__":
+    main()
+
